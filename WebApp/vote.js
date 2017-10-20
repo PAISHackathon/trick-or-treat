3058597064c00b37813a26e3cd2ecaf3ceacb88a
Khadilkar <mihir.khadilkar@rakuten.com>
2017-10-20 16:42:53 +0900

    Fixed options

--- WebApp/vote.js
+++ WebApp/vote.js
@@ -9,8 +9,8 @@ jQuery(document).ready(function () {
 			                	$('#description').val(data["topicDesc"]);
 			                	var options = data["options"]
 			                	var markup = "<option value='"+options[0]+"'>"+options[0]+"</option>" +
-			                					"<option value='"+options[1]+"'>"+options[1]+"</option>"
-			                					"<option value='"+options[2]+"'>"+options[2]+"</option>"
+			                					"<option value='"+options[1]+"'>"+options[1]+"</option>" +
+			                					"<option value='"+options[2]+"'>"+options[2]+"</option>" +
 			                					"<option value='"+options[3]+"'>"+options[3]+"</option>"
 			                			$("#options").append(markup);
 			                },
