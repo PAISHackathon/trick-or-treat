c59a49907677fd6b7c34d6c4cffeaebb4db049b1
Harshil Patel <ts-harshil.patel@rakuten.com>
2017-10-20 15:05:59 +0900

    Fixed Create Topic

--- WebApp/createTopic.js
+++ WebApp/createTopic.js
@@ -2,25 +2,26 @@
 
 function submitTopic() {
   var options = [];
-  options[0] = $('#opt1').html();
-  options[1] = $('#opt2').html();
-  options[2] = $('#opt3').html();
-  options[3] = $('#opt4').html()
+  options[0] = $('#opt1').val();
+  options[1] = $('#opt2').val();
+  options[2] = $('#opt3').val();
+  options[3] = $('#opt4').val();
   $.ajax({
         url: 'http://localhost:3001/topic',
         type: 'post',
         contentType: 'application/json',
         data: JSON.stringify({
-            topicTitle: $('#topicTitle').html(),
-            topicDesc: $('#topicDesc').html(),
-            startDate: $('#startDate').html(),
-            endDate: $('#endDate').html(),
+            topicTitle: $('#topicTitle').val(),
+            topicDesc: $('#topicDesc').val(),
+            startDate: $('#startDate').val(),
+            endDate: $('#endDate').val(),
             options: options,
             userId: '0xjhdsakdhaskjdasjdhsajiodjasld'
         }),
         dataType: 'json',
         success: function (data) {
             alert('Topic Created Successfully.')
+            window.location = 'SelectTopic.html'
         },
         error: function (response) {
             alert('Error in creating new Topic')
